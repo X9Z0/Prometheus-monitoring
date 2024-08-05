73a979f526a9d7d9e1f74815e9f79009c3c61de6
X9Z0 <x9z0cofd@gmail.com>
2024-08-06 02:52:36 +0530

    trying to solve the histogram not showing when contenarized

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,7 @@ RUN npm install
 
 COPY . .
 
+RUN npm run build
+
 EXPOSE 3000
 CMD ["node", "dist/index.js"]
--- src/metrics/index.ts
+++ src/metrics/index.ts
@@ -1,3 +1,41 @@
+// import { NextFunction, Request, Response } from "express";
+// import { requestCounter } from "./requestCount";
+// import { activeRequestGauge } from "./requestGauge";
+// import { httpRequestDurationMicroseconds } from "./histogram";
+
+// export const metricsMiddleware = (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   const startTime = Date.now();
+//   activeRequestGauge.inc();
+
+//   res.on("finish", function () {
+//     const endTime = Date.now();
+//     console.log(`Request took ${endTime - startTime}ms`);
+
+//     // Increment request counter
+//     requestCounter.inc({
+//       method: req.method,
+//       route: req.route ? req.route.path : req.path,
+//       status_code: res.statusCode,
+//     });
+
+//     httpRequestDurationMicroseconds.observe(
+//       {
+//         method: req.method,
+//         route: req.route ? req.route.path : req.path,
+//         code: res.statusCode,
+//       },
+//       endTime - startTime
+//     );
+
+//     activeRequestGauge.dec();
+//   });
+//   next();
+// };
+
 import { NextFunction, Request, Response } from "express";
 import { requestCounter } from "./requestCount";
 import { activeRequestGauge } from "./requestGauge";
@@ -11,6 +49,8 @@ export const metricsMiddleware = (
   const startTime = Date.now();
   activeRequestGauge.inc();
 
+  console.log(`Incoming request: ${req.method} ${req.path}`);
+
   res.on("finish", function () {
     const endTime = Date.now();
     console.log(`Request took ${endTime - startTime}ms`);
@@ -22,6 +62,7 @@ export const metricsMiddleware = (
       status_code: res.statusCode,
     });
 
+    console.log("Observing histogram");
     httpRequestDurationMicroseconds.observe(
       {
         method: req.method,
